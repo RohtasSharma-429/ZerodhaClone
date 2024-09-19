f3644bb481e6cf638d1c1dd592a1f8e37f2afe79
Vicky Sharma <ramansharma06834@gmail.com>
2024-09-19 23:06:01 +0530

    about page

--- src/landing_page/about/Hero.jsx
+++ src/landing_page/about/Hero.jsx
@@ -1,5 +1,52 @@
+import "./Hero.css";
+
 export default function Hero() {
-    return (
-        <h1>Hero Section</h1>
-    )
+  return (
+    <div className="container mt-5">
+      <div className="row p-3 hero-row-p3">
+        <div className="row about-h2">
+          <h2>
+            We pioneered the discount broking model in India.<br></br>
+            Now, we are breaking ground with our technology.
+          </h2>
+        </div>
+        <div className="row border-top about-row">
+          <div className="col-6 about-c6">
+            <p className="about-p">
+              We kick-started operations on the 15th of August, 2010 with the
+              goal of breaking all barriers that traders and investors face in
+              India in terms of cost, support, and technology. We named the
+              company Zerodha, a combination of Zero and &quot;Rodha&quot;, the
+              Sanskrit word for barrier.
+            </p>
+            <p className="about-p">
+              Today, our disruptive pricing models and in-house technology have
+              made us the biggest stock broker in India.
+            </p>
+            <p className="about-p">
+              Over 1+ Crore clients place millions of orders every day through
+              our powerful ecosystem of investment platforms, contributing over
+              15% of all Indian retail trading volumes.
+            </p>
+          </div>
+          <div className="col-6 about-c6">
+            <p className="about-p">
+              In addition, we run a number of popular open online educational
+              and community initiatives to empower retail traders and investors.
+            </p>
+            <p className="about-p">
+              <a href="#">Rainmatter</a>, our fintech fund and incubator, has
+              invested in several fintech startups with the goal of growing the
+              Indian capital markets.
+            </p>
+            <p className="about-p">
+              And yet, we are always up to something new every day. Catch up on
+              the latest updates on our <a href="#">blog</a> or see what the
+              media is <a href="#">saying about us.</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/landing_page/about/Team.jsx
+++ src/landing_page/about/Team.jsx
@@ -1,5 +1,216 @@
+import "./Team.css";
+
 export default function Team() {
-    return (
-        <h1>Team Section</h1>
-    )
+  return (
+    <div className="container">
+      <div className="row p-3 team-row">
+        <h3 className="text-center mb-2">People</h3>
+        <div className="row team-prof-row">
+          <div className="col-6">
+            <img
+              src="media/images/nithinKamath.jpg"
+              alt="Profile Img"
+              className="about-img text-center"
+            />
+            <p className="text-center fs-5 mt-4">Vicky Sharma</p>
+            <p className="text-center">Founder, CEO</p>
+          </div>
+          <div className="col-6 ">
+            <p className="about-pro-des">
+              Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+              hurdles he faced during his decade long stint as a trader. Today,
+              Zerodha has changed the landscape of the Indian broking industry.
+            </p>
+            <p className="about-pro-des">
+              He is a member of the SEBI Secondary Market Advisory Committee
+              (SMAC) and the Market Data Advisory Committee (MDAC).
+            </p>
+            <p className="about-pro-des">Playing basketball is his zen.</p>
+            <p>
+              Connect on <a href="#">Homepage</a> / <a href="#">TradingQnA</a> /{" "}
+              <a href="#">Twitter</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row team-bio-row">
+        <div className="col-4">
+          <img
+            src="media/images/nithinKamath.jpg"
+            alt="bio image"
+            className="bio-img"
+          />
+          <p className="fs-5">Vicky Sharma</p>
+          <p className="bio-name">Co-Founder & CFO</p>
+
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary bg-white bio-drop dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bio
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="p-3">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Veritatis eveniet hic veniam dolores nesciunt delectus quis rem
+                magnam ut iure, eum, quam rerum illo sit aut totam ipsa minima?
+                Consequuntur?
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-4">
+        <img
+            src="media/images/nithinKamath.jpg"
+            alt="bio image"
+            className="bio-img"
+          />
+          <p className="fs-5">Vicky Sharma</p>
+          <p className="bio-name">Co-Founder & CFO</p>
+
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary bg-white bio-drop dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bio
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="p-3">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Veritatis eveniet hic veniam dolores nesciunt delectus quis rem
+                magnam ut iure, eum, quam rerum illo sit aut totam ipsa minima?
+                Consequuntur?
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-4">
+        <img
+            src="media/images/nithinKamath.jpg"
+            alt="bio image"
+            className="bio-img"
+          />
+          <p className="fs-5">Vicky Sharma</p>
+          <p className="bio-name">Co-Founder & CFO</p>
+
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary bg-white bio-drop dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bio
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="p-3">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Veritatis eveniet hic veniam dolores nesciunt delectus quis rem
+                magnam ut iure, eum, quam rerum illo sit aut totam ipsa minima?
+                Consequuntur?
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row team-bio-row">
+        <div className="col-4">
+          <img
+            src="media/images/nithinKamath.jpg"
+            alt="bio image"
+            className="bio-img"
+          />
+          <p className="fs-5">Vicky Sharma</p>
+          <p className="bio-name">Co-Founder & CFO</p>
+
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary bg-white bio-drop dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bio
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="p-3">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Veritatis eveniet hic veniam dolores nesciunt delectus quis rem
+                magnam ut iure, eum, quam rerum illo sit aut totam ipsa minima?
+                Consequuntur?
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-4">
+        <img
+            src="media/images/nithinKamath.jpg"
+            alt="bio image"
+            className="bio-img"
+          />
+          <p className="fs-5">Vicky Sharma</p>
+          <p className="bio-name">Co-Founder & CFO</p>
+
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary bg-white bio-drop dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bio
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="p-3">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Veritatis eveniet hic veniam dolores nesciunt delectus quis rem
+                magnam ut iure, eum, quam rerum illo sit aut totam ipsa minima?
+                Consequuntur?
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-4">
+        <img
+            src="media/images/nithinKamath.jpg"
+            alt="bio image"
+            className="bio-img"
+          />
+          <p className="fs-5">Vicky Sharma</p>
+          <p className="bio-name">Co-Founder & CFO</p>
+
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary bg-white bio-drop dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bio
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="p-3">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Veritatis eveniet hic veniam dolores nesciunt delectus quis rem
+                magnam ut iure, eum, quam rerum illo sit aut totam ipsa minima?
+                Consequuntur?
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
